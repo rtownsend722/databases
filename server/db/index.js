94fab19d5892b7907da5b28ff8d4a1c4053ad80c
rtownsend722 <rebecca.townsend722@gmail.com>
2017-11-03 17:31:52 -0700

    Eventually connect to database, discover tests together

--- server/db/index.js
+++ server/db/index.js
@@ -5,3 +5,11 @@ var mysql = require('mysql');
 // and to the database "chat".
 
 
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'chat',
+  password: 'plantlife'
+});
+
+exports.connection.connect();
--- server/schema.sql
+++ server/schema.sql
@@ -1,14 +1,33 @@
-CREATE DATABASE chat;
+-- CREATE DATABASE chat;
 
 USE chat;
 
+-- CREATE TABLE users (
+--   id int PRIMARY KEY AUTO_INCREMENT,
+--   username text
+-- );
+
+-- CREATE TABLE rooms (
+--   id int PRIMARY KEY AUTO_INCREMENT,
+--   room text
+-- );
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id int PRIMARY KEY AUTO_INCREMENT,
+  message text,
+  user_id int,
+  room_id int,
+  FOREIGN KEY(user_id)
+    REFERENCES users(id)
+    ON DELETE CASCADE, 
+
+  FOREIGN KEY(room_id)
+    REFERENCES rooms(id)
+    ON DELETE CASCADE
 );
 
 /* Create other tables and define schemas for them here! */
-
-
 
 
 /*  Execute this file from the command line by typing:
